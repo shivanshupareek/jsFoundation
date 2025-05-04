fd3ce3e1c64dab43d40fe21ac31b1f25b713ef48
shivanshupareek <shivanshupareek.ps@gmail.com>
2025-05-04 10:23:02 +0530

    intro file added

---
+++ script.js
@@ -0,0 +1,14 @@
+function intro() {
+  document.querySelector("main section").innerHTML = `
+  <h4>
+      This script is an intro script which is designed to understand the structture of this project. 
+    </br>
+      This JS project is used to understand JS from the complete scratch and learn all the funcdamental concepts of javascript.
+    </br>
+      After going through this project, you will be able to have a grasp on all the foundational conepts of javascript and how javascript works.
+    </br>
+      Having such knowlegde will enable the learner to exponentially grow their developerment journey and learn frameworks and librarries with ease.
+    </br>
+      You can navigate through different js files in the recommended order in the html file for a more structured learning. 
+  </h4>`;
+}
